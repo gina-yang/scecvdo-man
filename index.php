23205b0679a95f62d09d6afbc0232d1450790dc0
Gina Yang <ginayang@usc.edu>
2019-07-19 16:07:22 -0700

    forgot to copy to php

--- index.php
+++ index.php
@@ -3,6 +3,15 @@
 <meta name="viewport"
 	content="width=device-width,height=device-height,initial-scale=1.0" />
 <style>
+@import "https://fonts.googleapis.com/css?family=Montserrat&display=swap";
+body {
+	font-family: 'Montserrat', sans-serif;
+}
+
+a:link {
+  text-decoration: none;
+}
+
 /* Smartphones (portrait and landscape) ----------- */
 @media only screen and (min-device-width : 320px) and (max-device-width
 	: 480px) { /* Styles */
@@ -24,7 +33,6 @@
 		height: 300px;
 		float: left;
 		display: inline-block;
-		font-family: tahoma;
 		vertical-align: top;
 	}
 }
@@ -56,7 +64,6 @@
 		height: 300px;
 		float: left;
 		display: inline-block;
-		font-family: tahoma;
 		vertical-align: top;
 	}
 }
@@ -70,23 +77,20 @@
 	screen and (min-device-pixel-ratio : 1.5) { /* Styles */
 }
 </style>
-<title>SCEC-VDO vtk</title>
+<title>SCEC-VDO</title>
+<link rel="icon" type="image/png" href="manual/images/vdologo.png">
 </head>
 
-<body border='0'>
-	<div style='width: 100%; height: 50px; background: #666;'
-		align='center'>
-		<div style='max-width: 1000px; height: 50px;' align='left'>
-			<img width='90px' height='50px' style='vertical-align: text-top;'
-				src='http://www.scec.org/themes/scec/logo.png'> <span
-				style='color: #FFF; font-style: italic;'>Southern California
-				Earthquake Center</span>
+<body>
+	<div style='width: 100%; height: 55px; background: #686868;' align='center'>
+		<div style='max-width: 1000px; height: 50px; vertical-align: middle;' align='center'>
+			<img width='90px' height='50px' src="http://www.scec.org/themes/scec/logo.png"/> 
 		</div>
 	</div>
 
 	<div style='width: 100%;' align='center'>
 		<div style='max-width: 1000px; height: 50px;' align='left'>
-			<img style='width: 100%; height: auto;' src='images/front.png'>
+			<img style='width: 100%; height: auto;' src='manual/images/scecvdofront.png'>
 
 			<div class='rcorners'>
 				<strong>Visualize all data as a system</strong> <br>
@@ -117,19 +121,17 @@
 					over time, easily navigating camera positions and turn data sets on
 					or off throughout the process. Combined with the new <b><i>Legends</i></b>
 					plugin, SCEC-VDO can produce refined movies without post-processing
-					software. <b><i>Statefulness</i></b> feature allows users to save
+					software. <b><i>Statefulness</i></b> allows users to save
 					their project and continue work at a later time. Collaboration is
 					as simple as sharing an XML file.
 			
 			</div>
 			<div class='rcorners'>
 				<strong>Downloads</strong>
-				<p>8/4/17 - Version 17.8 is the current stable releas. Just unzip
-					and run launch.sh from command prompt. Pre-release version 18.9
-					will be coming soon.</p>
+				<p>8/4/17 - Version 17.8 is the current stable release. Just unzip
+					and run launch.sh from command prompt. </br>
+					Version 18.9 is expected end of summer 2019.</p>
 				<p>
-				
-				
 				<li><a href='downloads/scecvdo_17.8_macosx_64bit.dmg'>Mac OS X
 						(316mb, dmg)</a></li>
 				<li><a href='downloads/scecvdo_17.8_macosx_64bit.zip'>Mac OS X
@@ -143,20 +145,16 @@
 					<a href='WebContent/manual/UserGuide.html'>User's Manual</a>
 				</P>
 			</div>
-			<div style='font-family: tahoma; display: inline-block;'>
+			<div style='display: inline-block;'>
 				<p>
-					<strong>About SCEC-VDO</strong><br> Researchers and undergraduate
-					interns at the Southern California Earthquake Center (SCEC) have
-					created a new 3-dimensional (3D) visualization software tool called
-					SCEC Virtual Display of Objects (SCEC-VDO). SCEC-VDO is written in
-					Java and uses the Visualization Toolkit (VTK) backend to render 3D
-					content. A prior version of SCECVDO, which began development in
-					2005 under the SCEC Undergraduate Studies in Earthquake Information
-					Technology internship, used the now unsupported Java3D library.
-					Replacing Java3D with the widely supported and actively developed
-					VTK libraries not only ensures that SCEC-VDO can continue to
-					function for years to come, but allows for the export of 3D scenes
-					to web viewers and popular software such as Paraview
+					<strong>About SCEC-VDO</strong><br> Researchers and interns at the <a href="http://www.scec.org">Southern California Earthquake Center (SCEC)</a> 
+				have built a 3D seismic data visualization software tool called the SCEC Virtual Display of Objects 
+				(SCEC-VDO). Written with a Swing graphical user interface, Java backend, and the Visualization Toolkit 
+				(VTK) to render 3D content, SCEC-VDO allows for the visualization of 3D earthquake and fault objects on 
+				maps and the creation of images and movies for analysis, presentation, and publication. A 
+				prior version, which began development in 2005, used the now unsupported Java3D library. Replacing Java3D 
+				with the widely supported and actively developed VTK libraries ensures that SCEC-VDO can maintain user 
+				accessibility and continue to function for years to come.
 				</p>
 			</div>
 
