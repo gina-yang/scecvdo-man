d0efcd803b6e848e00e49d4d4ae87022a4e4c065
Gina Yang <ginayang@usc.edu>
2019-07-26 14:05:53 -0700

    Fixed boxes floating to left in mobile

--- index.php
+++ index.php
@@ -15,9 +15,9 @@
 			width: 270px;
 			margin: 5px;
 			height: 300px;
-			float: left;
 			display: inline-block;
 			vertical-align: top;
+			text-align: left;
 		}
 		
 		.buttons {
@@ -44,8 +44,6 @@
 		a {
 			text-decoration: none;
 		}
-		
-		
 	</style>
 <title>SCEC-VDO</title>
 <link rel="icon" type="image/png" href="manual/images/vdologo.png">
@@ -56,64 +54,64 @@
 		<a href="https://www.scec.org" target="_blank"><img style="width: 90px; height: 50px; padding: 10px;" src="http://www.scec.org/themes/scec/logo.png"/></a>
 	</header>
 
-	<div style='width: 100%;' align='center'>
-		<div style='max-width: 1000px; height: 50px;' align='left'>
+	<div style="width: 100%;" align='center'>
+		<div style="max-width: 1000px; height: 50px;">
 			<img style='width: 100%; height: auto;' src='manual/images/scecvdofront.png'>
 			<div class="btn" align="center">
 				<a class="buttons" href="#">Download v18.9</a>
 				<a class="buttons" href="manual/UserGuide.html">&nbsp;&nbsp;User Manual&nbsp;&nbsp;</a>
 			</div>
-			<div style='display: inline-block; text-align: center;'>
+			<div style="display: inline-block; text-align: center;">
 				<h2>About SCEC-VDO</h2>
 				<p>Researchers and interns at the <a href="http://www.scec.org">Southern California Earthquake Center (SCEC)</a> 
 				have built a seismic data visualization software tool called the SCEC Virtual Display of Objects 
 				(SCEC-VDO). Written with a Swing graphical user interface, Java backend, and the Visualization Toolkit 
 				(VTK) to render 3D content, SCEC-VDO allows for the visualization of 3D earthquake and fault objects on 
-				maps and the creation of images and movies for analysis, presentation, and publication. A 
-				prior version, which began development in 2005, used the now unsupported Java3D library. Replacing Java3D 
-				with the widely supported and actively developed VTK libraries ensures that SCEC-VDO can maintain user 
-				accessibility and functionality for years to come. </br></br></br>
+				maps and the creation of images and movies for analysis, presentation, and publication. </br></br></br>
 				</p>
 			</div>
-			<div class='rcorners'>
-				<b>Visualize data</b>
-				<ul>
-					<li>Surface maps
-					<li>ShakeMaps
-					<li>Community fault models
-					<li>WGCEP fault models
-					<li>Earthquake event catalogs
-					<li>Hazus events
-					<li>Earthquake simulators
-					<li>UCERF3 fault system ruptures
-				</ul>
+			<div class="boxes" align="center">
+				<div class='rcorners'>
+					<b>Visualize data</b>
+					<ul>
+						<li>Surface maps
+						<li>ShakeMaps
+						<li>Community fault models
+						<li>WGCEP fault models
+						<li>Earthquake event catalogs
+						<li>Hazus events
+						<li>Earthquake simulators
+						<li>UCERF3 fault system ruptures
+					</ul>
+				</div>
+				<div class='rcorners'>
+					<b>Movies and collaboration made easy</b>
+					<p>
+						<b><i>Timeline</i></b> allows users to intuitively animate data
+						over time and navigate camera positions and turn data sets on
+						or off throughout the process. Combined with the new <b><i>Legends</i></b>
+						plugin, SCEC-VDO can produce refined movies without post-processing
+						software. <b><i>Statefulness</i></b> allows users to save
+						their project and continue work at a later time. Collaboration is
+						as simple as sharing an XML file.
+				</div>
+				<div class='rcorners'>
+					<b>Downloads</b>
+					<p>8/4/17 - Version 17.8 is the current stable release. Just unzip
+						and run launch.sh from command prompt. </br>
+						Version 18.9 is expected end of summer 2019.</p>
+					<p>
+					<li><a href='downloads/scecvdo_17.8_macosx_64bit.dmg'>Mac OS X
+							(316mb, dmg)</a></li>
+					<li><a href='downloads/scecvdo_17.8_macosx_64bit.zip'>Mac OS X
+							(316mb, zip)</a></li>
+					<li><a href='downloads/scecvdo_17.8_windows_64bit.zip'>Windows 64bit
+							(320mb, zip)</a></li>
+					<li><a href='downloads/scecvdo_17.8_linux_64bit.zip'>Linux (410mb,
+							zip)</a></li>
+				</div>
 			</div>
-			<div class='rcorners'>
-				<b>Movies and collaboration made easy</b>
-				<p>
-					<b><i>Timeline</i></b> allows users to intuitively animate data
-					over time and navigate camera positions and turn data sets on
-					or off throughout the process. Combined with the new <b><i>Legends</i></b>
-					plugin, SCEC-VDO can produce refined movies without post-processing
-					software. <b><i>Statefulness</i></b> allows users to save
-					their project and continue work at a later time. Collaboration is
-					as simple as sharing an XML file.
-			</div>
-			<div class='rcorners'>
-				<b>Downloads</b>
-				<p>8/4/17 - Version 17.8 is the current stable release. Just unzip
-					and run launch.sh from command prompt. </br>
-					Version 18.9 is expected end of summer 2019.</p>
-				<p>
-				<li><a href='downloads/scecvdo_17.8_macosx_64bit.dmg'>Mac OS X
-						(316mb, dmg)</a></li>
-				<li><a href='downloads/scecvdo_17.8_macosx_64bit.zip'>Mac OS X
-						(316mb, zip)</a></li>
-				<li><a href='downloads/scecvdo_17.8_windows_64bit.zip'>Windows 64bit
-						(320mb, zip)</a></li>
-				<li><a href='downloads/scecvdo_17.8_linux_64bit.zip'>Linux (410mb,
-						zip)</a></li>
-			</div>
+			
 		</div>
 	</div>	
 </body>
